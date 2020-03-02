08bb5e5bcbed924ccfb2e756a283010873c75579
Joaquin Cid <joaqcid@gmail.com>
2020-03-02 08:47:57 -0300

    a test

--- src/app/modules/forms/forms.module.ts
+++ src/app/modules/forms/forms.module.ts
@@ -2,13 +2,14 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PriceFormControlComponent } from './price-form-control/price-form-control.component';
-import { ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule, FormsModule as TemplateFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     PriceFormControlComponent,
   ],
   imports: [
+    TemplateFormsModule,
     ReactiveFormsModule,
     CommonModule,
     NgbModule,
